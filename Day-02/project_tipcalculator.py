f3c1faeb976dd06b9f82aa442bea91195184644a
buchepalli-ramana-2 <154529746+buchepalli-ramana-2@users.noreply.github.com>
2025-02-12 10:11:20 +0000

    project tip calculator completed

--- Day-02/project_tipcalculator.py
+++ Day-02/project_tipcalculator.py
@@ -9,3 +9,10 @@ Ask user to enter the bill amount,give options % of tip want to give and how man
 program has to give amount of each person should pay.
 
 '''
+print("Welcome to the Tip Calculator!!")
+bill_amount = int(input("What was total bill amount? $ "))
+tip = int(input(f"How much tip would you like give? 10 12 or 15? "))
+people= int(input(f"How many people split the bill? "))
+each_pay = (bill_amount + (bill_amount * (tip/100)))/people
+each_pay = round(each_pay,2)
+print(f"Each person should pay: ${each_pay}")
